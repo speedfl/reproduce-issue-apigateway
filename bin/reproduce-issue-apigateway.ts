9abcd167544149f8b03c4e71d469a450e9efda39
Geoffrey MUSELLI <gmuselli@ncelrnd2527.nce.amadeus.net>
2021-03-12 14:16:56 +0100

    Initial commit

---
+++ bin/reproduce-issue-apigateway.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { ReproduceIssueApigatewayStack } from '../lib/reproduce-issue-apigateway-stack';
+
+const app = new cdk.App();
+new ReproduceIssueApigatewayStack(app, 'ReproduceIssueApigatewayStack');
